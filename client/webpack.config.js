a80ed616e05b498402df34a24995c0b4180444c5
Mike Rugh <mike.rugh@gmail.com>
2022-10-27 16:17:24 -0700

    adds routes, starts webpack, install, and sws

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -21,7 +22,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
      
       // Injects our custom service worker
@@ -31,12 +32,13 @@ module.exports = () => {
       }),
 
       // Creates a manifest.json file.
+      new GenerateSW(),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'TextaPad',
+        short_name: 'Text Editor',
+        description: 'Edit some text!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
